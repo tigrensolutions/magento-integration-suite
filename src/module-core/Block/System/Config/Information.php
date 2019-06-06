7510bcbf2336e33c499bebbd014ee85af63433d0
Kai <toan@azebiz.com>
2019-06-06 10:33:24 +0700

    - Update Tigren's information

--- src/module-core/Block/System/Config/Information.php
+++ src/module-core/Block/System/Config/Information.php
@@ -77,15 +77,35 @@ class Information extends Fieldset
      */
     protected function _getInfo()
     {
-        $html = '<div class="support-info">';
-        $html .= '  <h3>Support Policy</h3>';
-        $html .= '  <p>Tigren provides 3-month free support for all of our extensions. We are not responsible for any bug or issue caused by your changes to our products. To report a bug, please send your email to: <a href="mailto:support@tigren.com" title="Tigren Support" target="_top">support@tigren.com</a></p>';
-        $html .= '  <h3>Tigren\'s Blog</h3>';
-        $html .= '  <p>We will be updating this blog on a regular basis to include you in new thinking and ideas emerging at Tigren, as well as to keep you updated with what’s going on in the e-commerce world. The blog is full with industry news, tutorials, hot releases, updates, promotions and so on. Let’s visit our blog to be kept updated!</p>';
-        $html .= '  <h3>Follow Us</h3>';
-        $html .= '  <div class="tigren-follow"><ul><li class="facebook"><a href="https://www.facebook.com/TigrenSolutions/" title="Facebook" target="_blank"><img src="' . $this->getViewFileUrl('Tigren_Core::images/facebook.png') . '" alt="Facebook"/></a></li><li class="twitter"><a href="https://twitter.com/Tigren5" title="Twitter" target="_blank"><img src="' . $this->getViewFileUrl('Tigren_Core::images/twitter.png') . '" alt="Twitter"/></a></li></ul></div>';
-        $html .= '</div>';
-
-        return $html;
+        return <<<HTML
+    <div class="support-info">
+        <p>Founded in 2012, Tigren is one of the top Magento agencies who enable e-commerce success for merchants by offering end-to-end Magento solutions, ranging from consulting, development to support - at an affordable price.</p>
+        <br/>
+        <p>At Tigren, we take pride in the quality of our work. We are committed to delivering transparent process, effective communication, comprehensive requirement analysis, short feedback loop, and fast issue resolution.</p>
+        <br/>
+        <h3>Our Outstanding Magento Services:</h3>
+        <br/>
+        <ul style="padding-left: 20px;">
+            <li class="item"><a href="https://www.tigren.com/our-services/magento-development/" title="Magento website development">Magento website development</a></li>
+            <li class="item"><a href="https://www.tigren.com/our-services/magento-migration-service/" title="Magento 2 migration">Magento 2 migration</a></li>
+            <li class="item"><a href="https://www.tigren.com/our-services/magento-mobile-app-development/" title="Magento mobile app development">Magento mobile app development</a></li>
+            <li class="item"><a href="https://www.tigren.com/our-services/magento-2-pwa-theme-integration/" title="Magento 2 PWA integration">Magento 2 PWA integration</a></li>
+            <li class="item"><a href="https://www.tigren.com/magento-2-extensions/" title="Magento extension development">Magento extension development</a></li>
+            <li class="item"><a href="https://www.tigren.com/our-services/magento-support-services/" title="Magento monthly support">Magento monthly support</a></li>
+        </ul>
+        <br/>
+        <h3>Why Us?</h3>
+        <br/>
+        <ul style="padding-left: 20px;">
+            <li class="item">Quality products followed by amazing after-service support</li>
+            <li class="item">Fast project delivery, always on-time or before deadlines</li>
+            <li class="item">Affordable price, satisfying even tight budgets</li>
+        </ul>
+        <br/>
+        <p>Up to now, we have implemented hundreds of Magento projects for small-to-large businesses, from various industries and markets, with proven successes. We always try to put ourselves in our clients’ shoes, to understand their pain points and suggest the most proper solutions.</p>
+        <br/>
+        <p><a href="https://www.tigren.com/contact/" title="Contact Us">Contact us</a> now to get free consultation and estimate for your project!</p>
+    </div>
+HTML;
     }
 }
