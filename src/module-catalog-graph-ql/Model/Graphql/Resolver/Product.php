9bb58fb5d89a4a342eb226457f7aca745d3ed1b7
loky <loky@tigren.com>
2019-10-09 21:39:32 +0700

    update catalog graphql

---
+++ src/module-catalog-graph-ql/Model/Graphql/Resolver/Product.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tigren\CatalogGraphQl\Model\Graphql\Resolver;
+
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Catalog\Model\ProductFactory;
+
+/**
+ * @inheritdoc
+ */
+class Product implements ResolverInterface
+{
+
+    /**
+     * @var ProductFactory
+     */
+    private $productFactory;
+
+    /**
+     * @param ProductFactory $productFactory
+     */
+    public function __construct(
+        ProductFactory $productFactory
+    ) {
+        $this->productFactory = $productFactory;
+    }
+    /**
+     * @inheritdoc
+     */
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
+    {
+        $productId = $this->getProductId($args);
+        $data = $this->getProductData($productId);
+
+        return $data;
+    }
+
+    /**
+     * @param array $args
+     * @return int
+     * @throws GraphQlInputException
+     */
+    private function getProductId(array $args): int
+    {
+        if (!isset($args['id'])) {
+            throw new GraphQlInputException(__('"Product id should be specified'));
+        }
+
+        return (int)$args['id'];
+    }
+
+    /**
+     * @param int $productId
+     * @return array
+     * @throws GraphQlNoSuchEntityException
+     */
+    private function getProductData(int $productId): array
+    {
+        $product = $this->productFactory->create()->load($productId);
+        if (!$product->getId()) {
+            throw new GraphQlNoSuchEntityException(__($e->getMessage()), $e);
+        }
+        $productData = $product->getData();
+        $productData['model'] = $product;
+        return [
+            'item' => $productData
+        ];
+    }
+
+}
